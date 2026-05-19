780e374e4fb76f592ada7a2bcf0dea1fcb4ab8b0
Sam <sam.koehler@mail.utoronto.ca>
2026-05-19 14:49:26 -0400

    branch1 commit to remote

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         // TODO complete this psvm main method to replicate the timing experiment from adts.py.
         //      Note: this will be client code of the various other classes needing to be written.
         // What doe client code mean???? - what about employee code lol
+
+        // This is an edit from branch 1!
     }
 }
