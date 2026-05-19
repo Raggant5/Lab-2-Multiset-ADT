51a4b1378003e2c4f5d3e3a34cb6ad1e12dbf5e6
Sam <sam.koehler@mail.utoronto.ca>
2026-05-19 15:02:58 -0400

    Testing second commit on branch 2 after -u

--- src/Tree.java
+++ src/Tree.java
@@ -2,4 +2,5 @@ public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 
     // This is branch2! My job is Tree.java. I have completed it.
+    // Testing second push
 }
